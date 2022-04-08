a394421b1dcf934905f6e132a4265e5d5d21def8
Luis Liguori <lugaliguori@gmail.com>
2022-04-08 15:54:17 -0400

    origin test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http, {
   cors: {
-    origin: "http://localhost:8100",
+    origin: "*",
     allowEIO3: true, // false by default,
     methods: ["GET", "POST"]
   }
